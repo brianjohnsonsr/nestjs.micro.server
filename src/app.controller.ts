637fded01a6f235e363af52564f898d4df1c57f7
Brian Johnson <b.johnson@fan-gene.com>
2019-08-11 11:50:57 -0400

    finish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Logger, Post, Body } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -9,10 +10,10 @@ export class AppController {
   // Inject the math service
   constructor(private mathService: MathService) {}
 
-  // Map the 'POST /add' route to this method
-  @Post('add')
+  // Define the message pattern for this method
+  @MessagePattern('add')
   // Define the logic to be executed
-  async accumulate(@Body('data') data: number[])  {
+  async accumulate(data: number[])  {
     this.logger.log('Adding ' + data.toString()); // Log something on every call
     return this.mathService.accumulate(data); // use math service to calc result & return
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+// Create a logger instance
+const logger = new Logger('Main');
+
+// Create the microservice options object
+const microserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: '127.0.0.1',
+    port: 8877,
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  app.listen(() => {
+    logger.log('Microservice is listening...');
+  });
 }
 bootstrap();
